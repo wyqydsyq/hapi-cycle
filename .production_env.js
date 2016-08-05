3da51c2523dede45d543f5fbd23545b8fa4f89b1
Damon Poole <pyrokinetiq@gmail.com>
2016-08-05 14:20:16 +1000

    Made env configs use PORT env var if present

--- .production_env.js
+++ .production_env.js
@@ -17,7 +17,7 @@ var conf = require(path.join(__dirname, 'package.json')),
 	version = conf.version,
 	env = 'production',
 	hostname = typeof (conf.env[env].hostname != 'undefined') ? conf.env[env].hostname : 'localhost',
-	port = typeof (conf.env[env].port != 'undefined') ? conf.env[env].port : 80,
+	port = process.env.PORT || typeof (conf.env[env].port != 'undefined') ? conf.env[env].port : 80,
 	host = hostname + ':' + port,
 	branch = getBranch();
 
