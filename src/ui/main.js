757548677889ac219a2e223611d212f0ba4d9ba2
Damon Poole <pyrokinetiq@gmail.com>
2016-08-19 15:16:02 +1000

    Updated deps

--- src/ui/main.js
+++ src/ui/main.js
@@ -2,8 +2,8 @@ import xs from 'xstream'
 import Routes from './routes'
 
 function Main (sources) {
-	let routes$ = sources.Router.define(Routes).remember(),
-		page$ = routes$.map(({path, value}) => value(sources)).debug('page:')
+	let routes$ = sources.Router.define(Routes),
+		page$ = routes$.map(({path, value}) => value(sources)).remember()
 
 	return {
 		DOM: page$.map(page => page.DOM).flatten(),
