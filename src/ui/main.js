e051816320608dee3a9007cfda9395e8842c0176
Damon Poole <pyrokinetiq@gmail.com>
2016-08-19 15:37:03 +1000

    added dropRepeats

--- src/ui/main.js
+++ src/ui/main.js
@@ -1,9 +1,14 @@
 import xs from 'xstream'
+import dropRepeats from 'xstream/extra/dropRepeats'
 import Routes from './routes'
 
+function routedComponent (sources) {
+	return ({path, value}) => value(Object.assign({}, sources, {Router: sources.Router.path(path)}))
+}
+
 function Main (sources) {
-	let routes$ = sources.Router.define(Routes),
-		page$ = routes$.map(({path, value}) => value(sources)).remember()
+	let routes$ = sources.Router.define(Routes).compose(dropRepeats((a, b) => a.path == b.path)),
+		page$ = routes$.map(routedComponent(sources)).remember()
 
 	return {
 		DOM: page$.map(page => page.DOM).flatten(),
